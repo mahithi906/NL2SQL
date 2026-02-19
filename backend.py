a92776316918d175f0d6fe2a668a3e0fa6663a2a
mahithi906 <tanguturi.mahithi.reddy@gds.ey.com>
2026-02-19 10:19:50 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -11,10 +11,8 @@ class QueryRequest(BaseModel):
 
 @app.post("/nl2sql")
 def nl2sql(req: QueryRequest):
-    # TODO: Run your NL→SQL model here
     sql = "SELECT * FROM customers LIMIT 5"
 
-    # TODO: Execute SQL on DB
     df = pd.DataFrame({"id": [1, 2, 3], "name": ["Alice", "Bob", "Charlie"]})
 
     return {"sql": sql, "rows": df.to_dict(orient="records")}
